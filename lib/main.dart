f789be05c3ce3275e99f77c6be67cf2a42401c71
Ahmed <you@example.com>
2024-11-12 09:08:41 +0200

    blue

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class birthday_card extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
               home:Scaffold(
-                backgroundColor: Colors.black,
+                backgroundColor: Colors.blue,
                 body: Center(
                   child: Image(image: AssetImage('lib/Imags/5x7in_Birthday-Card_v1_500x.webp')),
                 ),
